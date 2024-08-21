03da5131dc3a1e17d9ca0355f9dd58636d084d68
Andrew Liu <liua@bu.edu>
2024-08-21 11:17:35 -0400

    skills

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Intro from './Intro/Intro';
 import Experiences from './Experiences/Experiences';
 import Projects from './Projects/Projects';
 import BookReviews from './BookReviews/BookReviews';
+import Skills from './Skills/Skills';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Experiences />
       <Projects />
       <BookReviews />
+      <Skills />
     </div>
   );
 }
---
+++ src/Skills/Skills.tsx
@@ -0,0 +1,40 @@
+import './Skills.css'
+
+export default function Skills() {
+    return (
+        <section id="skills">
+            <div className="skills">
+                <p>
+                    <span>
+                        <h1>Key Skills</h1>
+                    </span>
+                    <span>
+                        <h2>Languages</h2>
+                        <ul>                
+                            <li>Java</li> 
+                            <li>JavaScript</li>
+                            <li>TypeScript</li>
+                            <li>HTML </li>
+                            <li>CSS </li>
+                            <li>Python </li>
+                            <li>SQL </li>
+                            <li>R </li>
+                        </ul>
+                    </span>
+                    <span>
+                        <h2>Dev Tools</h2>
+                        <ul>
+                            <li>Git </li>
+                            <li>Github </li>
+                            <li>VS Code </li>
+                            <li>Postman </li>
+                            <li>MongoDB </li>
+                            <li>Django </li>
+                            <li>Docker</li>
+                        </ul>
+                    </span>
+                </p>
+            </div>
+        </section>
+    )
+}
